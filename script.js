b7d396dfcdbc5d2aa4c04b3354b5ff24ed03a5d7
SwarnaSkyla <117882181+SwarnaSkyla@users.noreply.github.com>
2023-06-02 20:40:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// myProducts.filter((item)=>item.title.includes(search.value))
+
+// myCartProductArray = myProducts.filter((item)=> myCartIDs.includes(item.id))
+
+document.getElementById("login-btn").addEventListener("click",()=>{
+    window.location.href="./login/index.html"
+})
+
+document.getElementById("signup-btn").addEventListener("click",()=>{
+    window.location.href="./signup/index.html"
+})
